293501e5b50d6d62bd60b3f7de8e883eb47b558e
David-TC13 <115069379+David-TC13@users.noreply.github.com>
2023-02-22 07:43:02 +0100

    uploading

--- src/scrp.py
+++ src/scrp.py
@@ -2,11 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 
 def extrc(url):
+    
     """Applies BeatifulSoup to a url, creating a parser instance able to parse invalid markup"""
     soup = BeautifulSoup(requests.get(url).content, "html.parser")
     return soup
 
 def clearance_date(love_island_date):
+    
     """Clears up the data obtained looking for the date"""
     lst_date=[ i.getText().replace(',', '').replace('\n', ' ').split() for i in love_island_date]
     for i in lst_date:
@@ -14,11 +16,13 @@ def clearance_date(love_island_date):
     return lst_date
 
 def def_year(lst_date):
+    
     """Creates a list of the years"""
     year =[i[2] for i in lst_date]
     return year
 
 def def_month(lst_date):
+    
     """Creates a list of the months"""
     month =[i[0] for i in lst_date]
     return month
